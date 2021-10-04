a4a02b7f3881cd1cb1e339164c237b20836ae38e
viewipada <60265499+viewipada@users.noreply.github.com>
2021-10-05 00:45:03 +0700

    [bug] สร้าง setCheckboxValue(bool) ใน baggageViewModel และเก็ล selectedList ให้ถูกต้อง

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:trip_planner/src/notifiers/baggage_notifier.dart';
-import 'package:trip_planner/src/view/screens/baggage.dart';
-
+import 'package:trip_planner/src/view/screens/baggage_page.dart';
+import 'package:trip_planner/src/view_models/baggage_view_model.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => BaggageNotifire()),
-      ],
-      child: MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'EZtrip',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => BaggageViewModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'EZtrip',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: BaggagePage(),
       ),
-      home: Baggage(),
     );
   }
 }
