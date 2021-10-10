0cfa5a5d305186ee68127b61a4e988045ed2c5f0
viewipada <60265499+viewipada@users.noreply.github.com>
2021-10-11 01:44:58 +0700

    [flutter] ปรับแก้ให้ checkbox เลือกทั้งหมดไปอยู่ที่ leading ของ Appbar

--- lib/src/view/screens/baggage_page.dart
+++ lib/src/view/screens/baggage_page.dart
@@ -21,6 +21,30 @@ class _BaggagePageState extends State<BaggagePage> {
 
     return Scaffold(
       appBar: AppBar(
+        leading: Row(
+          children: [
+            Checkbox(
+              activeColor: Palette.PrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              value: baggageViewModel.checkboxValue,
+              onChanged: (value) {
+                baggageViewModel.setCheckboxValue(
+                  baggageViewModel.checkboxValue,
+                );
+              },
+            ),
+            Text(
+              'เลือกทั้งหมด',
+              style: TextStyle(
+                fontSize: 12,
+                color: Palette.AdditionText,
+              ),
+            ),
+          ],
+        ),
+        leadingWidth: 150,
         title: Text(
           "กระเป๋าเดินทาง",
           style: TextStyle(
@@ -32,184 +56,90 @@ class _BaggagePageState extends State<BaggagePage> {
         backgroundColor: Colors.white,
       ),
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 5, 20, 0),
-              child: Row(
-                children: [
-                  Checkbox(
-                    activeColor: Palette.PrimaryColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+            ListView(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 60),
+              children: baggageViewModel.baggageList.map((item) {
+                return Dismissible(
+                  key: UniqueKey(),
+                  direction: baggageViewModel.selectedList.contains(item)
+                      ? DismissDirection.none
+                      : DismissDirection.endToStart,
+                  onDismissed: (_) {
+                    setState(() {
+                      baggageViewModel.baggageList.remove(item);
+                    });
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 15),
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
                     ),
-                    value: baggageViewModel.checkboxValue,
-                    onChanged: (value) {
-                      baggageViewModel.setCheckboxValue(
-                        baggageViewModel.checkboxValue,
-                      );
+                  ),
+                  child: InkWell(
+                    onTap: () => {
+                      baggageViewModel.toggleSelection(
+                        baggageViewModel.selectedList.contains(item),
+                        item,
+                      ),
                     },
-                  ),
-                  Text(
-                    'เลือกทั้งหมดไปสร้างทริป',
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Palette.AdditionText,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Stack(
-                children: [
-                  ListView(
-                    padding: EdgeInsets.only(bottom: 60),
-                    children: baggageViewModel.baggageList.map((item) {
-                      return Dismissible(
-                        key: UniqueKey(),
-                        direction: baggageViewModel.selectedList.contains(item)
-                            ? DismissDirection.none
-                            : DismissDirection.endToStart,
-                        onDismissed: (_) {
-                          setState(() {
-                            baggageViewModel.baggageList.remove(item);
-                          });
-                        },
-                        background: Container(
-                          color: Colors.red,
-                          alignment: Alignment.centerRight,
-                          child: Padding(
-                            padding: EdgeInsets.only(right: 15),
-                            child: Icon(
-                              Icons.delete,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        child: InkWell(
-                          onTap: () => {
-                            baggageViewModel.toggleSelection(
-                              baggageViewModel.selectedList.contains(item),
-                              item,
-                            ),
-                          },
-                          child: Container(
-                            height: 110,
-                            child: Row(
+                    child: Container(
+                      height: 110,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 15),
+                            child: Stack(
                               children: [
-                                Padding(
-                                  padding: EdgeInsets.only(left: 15),
-                                  child: Stack(
-                                    children: [
-                                      Center(
-                                        child: Container(
-                                          child: ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                            child: Image(
-                                              width: 100,
-                                              height: 100,
-                                              fit: BoxFit.cover,
-                                              image:
-                                                  NetworkImage(item.imageUrl),
-                                            ),
-                                          ),
-                                        ),
+                                Center(
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Image(
+                                        width: 100,
+                                        height: 100,
+                                        fit: BoxFit.cover,
+                                        image: NetworkImage(item.imageUrl),
                                       ),
-                                      Visibility(
-                                        visible: baggageViewModel.selectedList
-                                            .contains(item),
-                                        child: Center(
-                                          child: Container(
-                                            width: 100,
-                                            height: 100,
-                                            alignment: Alignment.center,
-                                            decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(10),
-                                              color:
-                                                  Color.fromRGBO(0, 0, 0, 0.4),
-                                            ),
-                                            child: Container(
-                                              width: 24,
-                                              height: 24,
-                                              decoration: BoxDecoration(
-                                                shape: BoxShape.circle,
-                                                color: Palette.PrimaryColor,
-                                              ),
-                                              child: Center(
-                                                child: Text(
-                                                  (baggageViewModel.selectedList
-                                                              .indexOf(item) +
-                                                          1)
-                                                      .toString(),
-                                                  style: TextStyle(
-                                                    color: Colors.white,
-                                                  ),
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
+                                    ),
                                   ),
                                 ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: EdgeInsets.fromLTRB(10, 5, 15, 5),
+                                Visibility(
+                                  visible: baggageViewModel.selectedList
+                                      .contains(item),
+                                  child: Center(
                                     child: Container(
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Padding(
-                                            padding: EdgeInsets.only(
-                                              bottom: 5,
-                                            ),
-                                            child: Text(
-                                              item.name,
-                                              maxLines: 1,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 14,
-                                              ),
-                                            ),
-                                          ),
-                                          Text(
-                                            item.description,
-                                            overflow: TextOverflow.ellipsis,
-                                            maxLines: 2,
+                                      width: 100,
+                                      height: 100,
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Color.fromRGBO(0, 0, 0, 0.4),
+                                      ),
+                                      child: Container(
+                                        width: 24,
+                                        height: 24,
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Palette.PrimaryColor,
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            (baggageViewModel.selectedList
+                                                        .indexOf(item) +
+                                                    1)
+                                                .toString(),
                                             style: TextStyle(
-                                              fontSize: 12,
-                                              color: Palette.BodyText,
+                                              color: Colors.white,
                                             ),
                                           ),
-                                          Spacer(
-                                            flex: 2,
-                                          ),
-                                          Container(
-                                            decoration: BoxDecoration(
-                                              color: Palette.TagGrey,
-                                              borderRadius: BorderRadius.all(
-                                                Radius.circular(10),
-                                              ),
-                                            ),
-                                            padding:
-                                                EdgeInsets.fromLTRB(8, 3, 8, 3),
-                                            child: Text(
-                                              "ที่เที่ยว",
-                                              style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 10,
-                                              ),
-                                            ),
-                                          )
-                                        ],
+                                        ),
                                       ),
                                     ),
                                   ),
@@ -217,41 +147,97 @@ class _BaggagePageState extends State<BaggagePage> {
                               ],
                             ),
                           ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                  Positioned(
-                    height: 48,
-                    bottom: 5,
-                    left: 15,
-                    right: 15,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        baggageViewModel.selectedList
-                            .forEach((element) => print(element.name));
-                        print("-------------------- " +
-                            baggageViewModel.selectedList.length.toString() +
-                            " --------------------");
-                      },
-                      child: Text(
-                        'เริ่มสร้างทริป',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: Palette.PrimaryColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(10, 5, 15, 5),
+                              child: Container(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                        bottom: 5,
+                                      ),
+                                      child: Text(
+                                        item.name,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ),
+                                    Text(
+                                      item.description,
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 2,
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Palette.BodyText,
+                                      ),
+                                    ),
+                                    Spacer(
+                                      flex: 2,
+                                    ),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Palette.TagGrey,
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(10),
+                                        ),
+                                      ),
+                                      padding: EdgeInsets.fromLTRB(8, 3, 8, 3),
+                                      child: Text(
+                                        "ที่เที่ยว",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 10,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  )
-                ],
-              ),
+                  ),
+                );
+              }).toList(),
             ),
+            Positioned(
+              height: 48,
+              bottom: 5,
+              left: 15,
+              right: 15,
+              child: ElevatedButton(
+                onPressed: () {
+                  baggageViewModel.selectedList
+                      .forEach((element) => print(element.name));
+                  print("-------------------- " +
+                      baggageViewModel.selectedList.length.toString() +
+                      " --------------------");
+                },
+                child: Text(
+                  'เริ่มสร้างทริป',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Palette.PrimaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
