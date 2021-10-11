9dc8f33f95e2c91bf0cd486a43e753e220b79cc1
viewipada <60265499+viewipada@users.noreply.github.com>
2021-10-11 14:38:44 +0700

    [+] สร้างโมเดล BaggageResponse สำหรับหน้า baggage

---
+++ lib/src/models/response/baggage_response.dart
@@ -0,0 +1,35 @@
+class BaggageResponse {
+  int locationId;
+  String locationName;
+  String imageUrl;
+  String category;
+  String description;
+
+  BaggageResponse({
+    required this.locationId,
+    required this.locationName,
+    required this.imageUrl,
+    required this.category,
+    required this.description,
+  });
+
+  factory BaggageResponse.fromJson(Map<String, dynamic> json) {
+    return BaggageResponse(
+      locationId: json['locationId'],
+      locationName: json['locationName'],
+      imageUrl: json['imageUrl'],
+      category: json['category'],
+      description: json['description'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['locationId'] = this.locationId;
+    data['locationName'] = this.locationName;
+    data['imageUrl'] = this.imageUrl;
+    data['category'] = this.category;
+    data['description'] = this.description;
+    return data;
+  }
+}
