af3c68b98477bd3b8595d77d150b0e8192a35163
krataiiza55plus <tai.sophita@gmail.com>
2021-10-29 10:02:44 +0700

    [API] สร้างตารางในไฟล์ index.js

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -29,5 +29,9 @@ db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
 db.baggages = require("./baggage.model.js")(sequelize, Sequelize);
 db.locations = require("./location.model.js")(sequelize, Sequelize);
 db.reviews = require("./review.model.js")(sequelize, Sequelize);
+db.tripItems = require("./tripItem.model.js")(sequelize, Sequelize);
+db.trips = require("./trip.model.js")(sequelize, Sequelize);
+db.settingDuration = require("./settingDuration.model.js")(sequelize, Sequelize);
+db.user = require("./user.model.js")(sequelize, Sequelize);
 
 module.exports = db;
