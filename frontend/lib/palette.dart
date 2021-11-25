2b2cd159ff69333c1ce408b0c8d960c5d4d74aa3
viewipada <60265499+viewipada@users.noreply.github.com>
2021-11-25 21:54:34 +0700

    [flutter] แก้ไข conflict สี palette.dart

--- frontend/lib/palette.dart
+++ frontend/lib/palette.dart
@@ -36,6 +36,5 @@ class Palette {
   static const PeachColor = Color(0xffFF6464);
   static const LightOrangeColor = Color(0xffFFBD67);
   static const SelectedListTileColor = Color(0xffEBECED);
-  static const BorderInputColor = Color(0xffEBECED);
-  static const InfoText = Color(0xffA5ADBB);
+
 }
