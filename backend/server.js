189c47009a7a383fe25ec1f557222d492451b3de
krataiiza55plus <tai.sophita@gmail.com>
2022-04-18 00:01:38 +0700

    [API] set use cor server

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,12 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 db.sequelize.sync();
 
 // simple route
