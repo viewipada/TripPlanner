ae1678c50733d57fdafc0fe255f8a2a26188dca6
viewipada <60265499+viewipada@users.noreply.github.com>
2022-05-07 21:03:37 +0700

    [recommend] clear branch

--- frontend/android/app/src/main/kotlin/com/example/trip_planner/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.trip_planner
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- frontend/lib/assets.dart
+++
@@ -1,9 +0,0 @@
-class ImageAssets {
-  static const String logo = 'assets/images/logo.png';
-  static const String homeBanner = 'assets/images/home_banner.png';
-}
-class IconAssets {
-  static const String copyToEdit = 'assets/icons/copyToEdit.png';
-  static const String baggage = 'assets/icons/luggage.png';
-  static const String baggageAdd = 'assets/icons/luggage-add.png';
-}
