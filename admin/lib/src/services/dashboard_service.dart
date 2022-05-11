a8282406bd69f48e81b44a4fac2ca69c7a7c5331
viewipada <60265499+viewipada@users.noreply.github.com>
2022-05-11 19:01:53 +0700

    [flutter] เปลี่ยน baseUrl

--- admin/lib/src/services/dashboard_service.dart
+++ admin/lib/src/services/dashboard_service.dart
@@ -5,7 +5,7 @@ import 'package:admin/src/shared_pref.dart';
 import 'package:http/http.dart' as http;
 
 class DashboardService {
-  final String baseUrl = 'https://eztrip-backend.herokuapp.com';
+  final String baseUrl = 'http://localhost:8080';
 
   Future<List<LocationCardResponse>> getLocationBy(int category) async {
     List<LocationCardResponse> baggageList = [];
